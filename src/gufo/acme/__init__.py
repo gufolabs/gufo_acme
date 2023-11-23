786cdd9f6d1319107690a789bca48d57a0520c78
Dmitry Volodin <dv@nocproject.org>
2023-11-23 14:41:07 +0100

    0.4.0

--- src/gufo/acme/__init__.py
+++ src/gufo/acme/__init__.py
@@ -17,4 +17,4 @@ The package consists of the following modules:
 * [log][gufo.acme.log] - Logging utilities.
 * [types][gufo.acme.types] - Package public types.
 """
-__version__ = "0.3.0"
+__version__ = "0.4.0"
